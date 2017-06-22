1d4b5db1fa9d5f06ff27c95faf68378b44d612c3
pierre <pierre1@bbxmusic.com>
2017-06-21 19:30:08 -0500

    Fixed error in adding NTest (forgot to commit new project)

---
+++ WebApplication1.NTest/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using WebApplication1.Controllers;
+
+namespace WebApplication1.NTest
+{
+    [TestFixture]
+    public class HomeControllerNTest
+
+    {
+        [TestCase]
+        public void ControllerTest() { 
+            // Arrange
+            HomeController controller = new HomeController();            
+
+            // Assert
+            Assert.IsNotNull(controller);
+        }
+    }
+}
